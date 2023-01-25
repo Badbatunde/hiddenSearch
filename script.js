eea5f84b67c382c1a1204bace2d25f02a81c4a28
Badbatunde <hb.oyesile@gmail.com>
2023-01-25 07:46:45 +0100

    Created a function that toggles the active mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-s
+const input = document.querySelector('.input')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', toggleActive)
+
+function toggleActive() {
+    search.classList.toggle('active')
+    input.focus()
+}
